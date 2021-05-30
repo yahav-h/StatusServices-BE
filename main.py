4b5dd04060fb48c900b96422b4b187d9f17ee313
Yahav-N-Hoffman <yahavh@avanan.com>
2021-05-30 15:20:10 +0300

    update main module - add ping service check , fix edge cases in entries

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import subprocess, shlex
+from concurrent.futures import ThreadPoolExecutor, wait, FIRST_EXCEPTION
 from fastapi import FastAPI, Request, Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi.middleware.gzip import GZipMiddleware
-from backend.lambdas import Stamp, DateNow
-from backend.enums import AppState, AppRoutes
+from backend.lambdas import Stamp, DateNow, MakeTaskID
+from backend.enums import AppStates, AppRoutes
 from backend.datatypes import Tasks
 from backend.database import db_session, db_init
-from uuid import uuid5, NAMESPACE_URL
 
 
 app = FastAPI()
@@ -21,77 +22,109 @@ async def add_process_time_header(request: Request, call_next):
     return response
 
 
-@app.get(path=AppRoutes.Root.value)
-def root():
+@app.get(path=AppRoutes.Health.value)
+def health():
+    content = {
+        'status': AppStates.Success.value,
+        'timestamp': DateNow()
+    }
+    json = jsonable_encoder(obj=content)
+    return JSONResponse(content=json, status_code=200)
+
+
+async def ping_domain(address):
+    def ping(address):
+        cmd = f'ping -c 4 {address}'
+        args = shlex.split(cmd)
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+        proc.wait()
+        if proc.poll():
+            return False, proc.stdout.read()
+        else:
+            return True, proc.stdout.read()
     try:
-        content = {
-            'status': AppState.Success.value,
-            'timestamp': DateNow()
-        }
-        json = jsonable_encoder(obj=content)
-        return JSONResponse(content=json, status_code=200)
-    except Exception as e:
-        content = {
-            'status': AppState.Fail.value,
-            'timestamp': DateNow()
-        }
-        json = jsonable_encoder(obj=content)
-        return JSONResponse(content=json, status_code=400)
+        with ThreadPoolExecutor(max_workers=1, thread_name_prefix=f'PING_{address}') as executor:
+            future = executor.submit(ping, address)
+            state = wait(fs=[future], timeout=120, return_when=FIRST_EXCEPTION)
+            if state.done:
+                result = state.done.pop()
+            if result._state == 'Finished':
+                future.set_result(result._state)
+            obj = future.result()
+            return obj
+    except:
+        return False
 
 
 @app.post(path=AppRoutes.Ping.value)
-def task_ping(request: Request):
+async def task_ping(request: Request):
     service_ip = request.query_params.get('ip')
-    try:
-        task_id = uuid5(NAMESPACE_URL, f'{Stamp()}').hex
-        task_obj = Tasks(taskID=task_id, serviceIP=service_ip, taskStatus=AppState.Pending.value)
-        db_session.add(task_obj)
-        db_session.commit()
+    if not service_ip:
         content = {
-            'status': AppState.Pending.value,
+            'status': AppStates.Fail.value,
             'timestamp': DateNow(),
-            'taskID': task_id
-        }
-        json = jsonable_encoder(obj=content)
-        return JSONResponse(content=json, status_code=200)
-    except Exception as e:
-        content = {
-            'status': AppState.Fail.value,
-            'timestamp': DateNow()
         }
         json = jsonable_encoder(obj=content)
         return JSONResponse(content=json, status_code=400)
+
+    task_id = MakeTaskID()
+    task_obj = Tasks(taskID=task_id, serviceIP=service_ip, taskStatus=AppStates.Pending.value)
+
+    BooleanOrTuple = await ping_domain(service_ip)
+    if isinstance(BooleanOrTuple, bool):
+        task_obj.serviceStatus = AppStates.ServiceDown.value
+    elif isinstance(BooleanOrTuple, tuple):
+        task_obj.serviceStatus = AppStates.ServiceUp.value
+        task_obj.lastTimeCheck = DateNow()
+    task_obj.taskStatus = AppStates.Done.value
+
+    db_session.add(task_obj)
+    db_session.commit()
+
+    content = {
+        'status': AppStates.Success.value,
+        'timestamp': DateNow(),
+        'taskID': task_id
+    }
+    json = jsonable_encoder(obj=content)
+    return JSONResponse(content=json, status_code=200)
 
 
 @app.get(path=AppRoutes.Ping.value)
 def task_status(request: Request):
-    try:
-        task_id = request.query_params.get('taskid')
-        if not task_id:
-            content = {
-                'status': AppState.Fail.value,
-                'timestamp': DateNow(),
-                'message': 'Missing task id'
-            }
-            json = jsonable_encoder(obj=content)
-            return JSONResponse(content=json, status_code=400)
-
-        task_obj = Tasks.query.filter_by(id=task_id).first()
+    task_id = request.query_params.get('taskid')
+    if not task_id:
         content = {
-            task_id: task_ping.service_ip,
-            'status': AppState.Success.value,
-            'timestamp': DateNow()
+            'status': AppStates.Fail.value,
+            'timestamp': DateNow(),
+            'message': 'Missing task id'
         }
         json = jsonable_encoder(obj=content)
         return JSONResponse(content=json, status_code=400)
-
-    except Exception as e:
+    task_obj = Tasks.query.filter_by(taskID=task_id).first()
+    if not task_obj:
         content = {
-            'status': AppState.Fail.value,
-            'timestamp': DateNow()
+            'status': AppStates.Fail.value,
+            'timestamp': DateNow(),
+            'message': f'Task id \'{task_id}\' does not exists'
         }
         json = jsonable_encoder(obj=content)
-        return JSONResponse(content=json, status_code=400)
+        return JSONResponse(content=json, status_code=404)
+
+    service_ip = task_obj.serviceIP
+    service_status = task_obj.serviceStatus
+    last_time_checked = task_obj.lastTimeCheck
+    db_session.add(task_obj)
+    db_session.commit()
+
+    content = {
+        'status': service_status,
+        'timestamp': DateNow(),
+        'service': service_ip,
+        'serviceTimeCheck': last_time_checked
+    }
+    json = jsonable_encoder(obj=content)
+    return JSONResponse(content=json, status_code=400)
 
 
 if __name__ == '__main__':
