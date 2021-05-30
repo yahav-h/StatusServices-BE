33c57888e9fc1f135b825dc1af902a88dc0cd925
Yahav-N-Hoffman <yahavh@avanan.com>
2021-05-30 15:18:57 +0300

    add new lambda function MakeTaskID

--- backend/lambdas.py
+++ backend/lambdas.py
@@ -1,6 +1,8 @@
 from time import time
 from datetime import datetime
+from uuid import uuid5, NAMESPACE_URL
 
 
-DateNow = lambda: datetime.now().isoformat()
+DateNow = lambda epoch=False: datetime.now().isoformat() if not epoch else datetime(1970, 1, 1).isoformat()
 Stamp = lambda: time()
+MakeTaskID = lambda: uuid5(NAMESPACE_URL, DateNow()).hex
