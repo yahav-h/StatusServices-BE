2dd08bc72f9e66d14873f77c41aea051f85c381a
Yahav-N-Hoffman <yahavh@avanan.com>
2021-06-03 05:46:08 +0300

    add serviceName support to datatype ORM

--- backend/datatypes.py
+++ backend/datatypes.py
@@ -7,7 +7,8 @@ from backend.lambdas import DateNow
 class Tasks(Base):
     __tablename__ = "tasks_tbl"
     taskID = Column(Text(), primary_key=True, unique=True, nullable=False, autoincrement=False)
-    serviceIP = Column(Text(), unique=False, nullable=False)
+    serviceName = Column(Text(), unique=False, nullable=False)
+    serviceIP = Column(Text(), unique=False, nullable=True, default="N/a")
     taskStatus = Column(Text(), unique=False, nullable=False)
     serviceStatus = Column(Text(), unique=False, nullable=True)
     timeCreated = Column(Text(), unique=False, nullable=False)
@@ -15,11 +16,13 @@ class Tasks(Base):
 
     def __init__(self,
                  taskID=None,
+                 serviceName=None,
                  serviceIP=None,
                  taskStatus=None,
                  lastTimeCheck=None
                  ):
         self.taskID = taskID
+        self.serviceName = serviceName
         self.serviceIP = serviceIP
         self.taskStatus = taskStatus
         self.timeCreated = DateNow()
