a512452998c8493aab933d7465e2b7354b14d7aa
Yahav-N-Hoffman <yahavh@avanan.com>
2021-06-03 05:46:31 +0300

    remove unsed lambda function

--- backend/lambdas.py
+++ backend/lambdas.py
@@ -1,8 +1,6 @@
 from time import time
 from datetime import datetime
-from uuid import uuid5, NAMESPACE_URL
 
 
 DateNow = lambda epoch=False: datetime.now().isoformat() if not epoch else datetime(1970, 1, 1).isoformat()
 Stamp = lambda: time()
-MakeTaskID = lambda: uuid5(NAMESPACE_URL, DateNow()).hex
